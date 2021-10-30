1feca44eba1be7f1e9201abbfa16b1b66dafd06c
Sai Varun <karnati187@gmail.com>
2021-10-30 18:34:55 +0530

    a page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,36 @@
-function App( ) {
+const App = () => {
+
+  const chores = ["Item 1" , "Item 2" , "Item 3"]
+
+
   return (
     <div className="App">
-      <h2>hi</h2>     
-    </div>    
+      <nav>
+      <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Website</a>        
+      </nav>
+      <div className="container">
+      <ul>
+          {
+            chores.map(chore => <li>{chore}</li>)
+          }
+        </ul>
+
+
+      </div>
+      <footer>
+        <a href="#">Github</a>
+        <a href="#">Instagram</a>
+        <a href="#">Twitter</a>
+        <a href="#">Linkedin</a>
+
+      </footer>
+    </div >
   );
 }
+
+
 
 export default App;
